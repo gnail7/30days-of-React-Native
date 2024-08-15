89cce267526f62b53f4ce9cfcb8732c8f1290161
xuzhiliang <xuzhiliang@cubeenergy.com.cn>
2024-08-15 10:45:35 +0800

    项目初始化

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Alert } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Root = createStackNavigator()
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableHighlight
+        onPress={() => Alert.alert('Button Pressed!')}
+        underlayColor="gray"
+      >
+        <Text>Press Me</Text>
+      </TouchableHighlight>
     </View>
   );
 }
